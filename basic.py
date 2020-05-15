f685f76e20b1842dc7c584fb4d1caa405eb8f896
elmu <eltadjibaev@gmail.com>
2020-05-15 17:17:32 +0900

    test

---
+++ basic.py
@@ -0,0 +1,22 @@
+#how to use int(), type()
+""" birth_year = input('Birth year: ')
+print(type(birth_year))
+age = 2020 - int(birth_year)
+print(age) """
+
+"""course = 'Python for Beginers'
+print(course[0:3])
+print(course[0:])
+print(course[1:])
+print(course[:5])"""
+
+first = 'Elmu'
+last = 'Tad'
+msg = f'{first} {last} is a coder'
+print(msg)
+
+course = 'Python for Beginers'
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course.find('f'))
