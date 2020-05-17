cebf93443066d234dda2d87b7b0dd19e0eeca9ca
elmu <eltadjibaev@gmail.com>
2020-05-17 14:52:04 +0900

    Loop in Python

---
+++ loopInPython.py
@@ -0,0 +1,21 @@
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+for item in [1,2,3,4]:
+    print(item)
+
+for item in range(5):
+    print('item')
+
+for item in range(5, 10):
+    print('item')
+
+for item in range(5, 10, 2):
+    print('item')
+
+prices = [10,20,30]
+for item in prices:
+    pass
+
