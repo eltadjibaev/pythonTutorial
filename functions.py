ffaa9d3811c5bb85932297202e1369a29794ad71
elmu <eltadjibaev@gmail.com>
2020-05-18 14:52:15 +0900

    Functions in Python

--- functions.py
+++ functions.py
@@ -9,3 +9,22 @@ def greet_user(firstName, lastName):
     print(f'Hi there, {firstName} {lastName}')
 
 greet_user(lastName = 'Smith', firstName = 'John')
+
+# Return Statement
+def square(number):
+    return number * number
+
+result = square(3)
+print(result)
+
+# Exceptions
+try:
+    age = int(input('Age: '))
+    income = 2000
+    risk = income / age
+    print(age)
+except ZeroDivisionError:
+    print('Age cannot be 0')
+except ValueError:
+    print('Invalid value')
+
